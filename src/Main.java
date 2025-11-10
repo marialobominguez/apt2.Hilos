edc5e7b1a3eb094b47dd3eac5b2bf0dcd4e7119a
marialobominguez <marialobominguez@gmail.com>
2025-11-10 13:53:39 +0100

    he creado un ArrayList en el main para meter los pasajeros

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,21 @@
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        //Completar Codigo
+        //Creo las balsas:
         Balsa Acasta = new Balsa(1,0.5);
         Balsa Banff = new Balsa(2,1);
         Balsa Cadiz = new Balsa(3,2);
         Balsa Deimos = new Balsa(4,4);
         Balsa Expedición = new Balsa(5,8);
+
+        //Creo la lista en la que voy a añadir a los pasajeros
+        ArrayList<Pasajero> pasajerosBarco = new ArrayList<>();
+
+        
 
     }
 }
