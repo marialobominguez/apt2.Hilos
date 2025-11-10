007fdef56528d32df4810bcc6f7b787ad6714098
marialobominguez <marialobominguez@gmail.com>
2025-11-10 13:40:30 +0100

    he añadido al main las 5 balsas

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,10 @@ public class Main {
 
         //Completar Codigo
         Balsa Acasta = new Balsa(1,0.5);
+        Balsa Banff = new Balsa(2,1);
+        Balsa Cadiz = new Balsa(3,2);
+        Balsa Deimos = new Balsa(4,4);
+        Balsa Expedición = new Balsa(5,8);
 
     }
 }
