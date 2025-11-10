8e6c3945a6e6f81fcdc22ee4d4bf7f23f7bd6824
marialobominguez <marialobominguez@gmail.com>
2025-11-10 14:11:09 +0100

    he creado los pasajeros y los he metido en el ArrayList

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,25 @@ public class Main {
         //Creo la lista en la que voy a añadir a los pasajeros
         ArrayList<Pasajero> pasajerosBarco = new ArrayList<>();
 
-        
+        // Creo los pasajeros y los meto en el arraylist
+        int id = 1;
+        for (int i=1; i<=352;i++){
+            int prioridad = (int)(Math.random()*(5-1)+1);
+            /*
+            Math random -> lo multiplicamos por (numMax - numMin) y le
+                           sumamos 1 para que no empiece en el 0
+                           Como tira hacia abajo, hay que poner uno más
+                           en el máximo, y así podrán aparecer 4
+             */
+            if(prioridad==5){
+                prioridad=2; //si sale 5, quiero que sean adultos
+            }
+            Pasajero p = new Pasajero (id,prioridad);
+            pasajerosBarco.add(p);
+            id++;
+            System.out.println(p);
+        }
+
 
     }
 }
