49e886e5889a49e48280b9cc1db2e871a202ffbb
marialobominguez <marialobominguez@gmail.com>
2025-11-17 13:38:05 +0100

    hago la estructura en rescate de lo que quiero que haga y le modifico el constructor

--- src/Rescate.java
+++ src/Rescate.java
@@ -1,8 +1,20 @@
-public class Rescate implements Runnable{
+import java.util.ArrayList;
 
-    public Rescate(){}
+public class Rescate implements Runnable{
+    private boolean quedanPasajeros;
+
+    public Rescate(ArrayList<Pasajero> pasajeros){
+        Barco barco = new Barco(pasajeros);
+        quedanPasajeros = barco.hayPasajeros();
+    }
     @Override
     public void run() {
-        while(true){}
+        while(quedanPasajeros){
+            // mientras haya pasajeros vamos a
+            // embarcar
+            //ir a tierra (sleep)
+            //desembarcar y mostrar los pasajeros rescatados (toString)
+            // volvemos al barco (sleep)
+        }
     }
 }
