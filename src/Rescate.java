771dfa80bb092ad174d194f17d53e937ec8b1634
marialobominguez <marialobominguez@gmail.com>
2025-11-19 18:57:55 +0100

    he sacado todo lo que he implementado en el run() para hacer métodos sincronizados con cada uno de ellos (embarcar, navegandoATierra, desembarcar y volviendoABarco), a los que luego llamo en el método run()

--- src/Rescate.java
+++ src/Rescate.java
@@ -13,45 +13,60 @@ public class Rescate implements Runnable{
 
     @Override
     public void run() {
-        while (barco.hayPasajeros()){// mientras haya pasajeros vamos a
-            // embarcar = baja pasajero del barco y sube a la balsa
-            System.out.println("Embarcando pasajero(s) en la balsa "+balsa+"...");
-            for (int i = 0; i < balsa.getCapacidad(); i++) { //vamos a meter tantos pasajeros como permita la balsa
-                Pasajero p = barco.pasajeroPrioritario();
-                System.out.println("/t"+p.toString()+" sube a la balsa "+balsa);
-                balsa.subirPasajeroBalsa(p); //sube pasajero a la balsa y por lo tanto...
-                barco.bajarPasajerosBarco(p);//... baja del barco
-                System.out.println("------------------------------------");
-            }
+        while (barco.hayPasajeros()){// mientras haya pasajeros vamos a:
+            // 1_ embarcar = baja pasajero del barco y sube a la balsa
+            embarcar();
 
+            // 2_ ir a tierra (sleep)
+            navegandoATierra();
 
-            //ir a tierra (sleep)
-            System.out.println("Navegando a tierra firme...");
-            try {
-                Thread.sleep((int)(balsa.getTiempo()));
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+            //3_ desembarcar y mostrar los pasajeros rescatados (toString)
+            desembarcar();
+
+            // 4_ volvemos al barco (sleep)
+            volviendoABarco();
+
+        } //fin while (no quedan pasajeros)
+    } // fin run
+
+    public synchronized void embarcar(){
+        System.out.println("Embarcando pasajero(s) en la balsa "+balsa+"...");
+        for (int i = 0; i < balsa.getCapacidad(); i++) { //vamos a meter tantos pasajeros como permita la balsa
+            Pasajero p = barco.pasajeroPrioritario();
+            System.out.println("/t"+p.toString()+" sube a la balsa "+balsa);
+            balsa.subirPasajeroBalsa(p); //sube pasajero a la balsa y por lo tanto...
+            barco.bajarPasajerosBarco(p);//... baja del barco
             System.out.println("------------------------------------");
-
-            //desembarcar y mostrar los pasajeros rescatados (toString)
-            System.out.println("EN TIERRA FIRME");
-            System.out.println("Desembarcando pasajeros...");
-            System.out.println("Pasajeros salvados: ");
-            for (int i = 0; i < balsa.getCapacidad(); i++) {
-                System.out.println("/t"+balsa.getPasajeros().get(i)+" baja de la balsa "+balsa); //no tengo muy claro de que esto esté bien
-                balsa.bajarPasajeroBalsa(balsa.getPasajeros().get(i)); //tampoco lo tengo claro
-            }
-
-
-            // volvemos al barco (sleep)
-            System.out.println("La balsa "+balsa+" vuelve al barco a comprobar si quedan pasajeros.");
-            try {
-                Thread.sleep((int)(balsa.getTiempo()));
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-
         }
     }
+
+    public synchronized void navegandoATierra(){
+        System.out.println("Navegando a tierra firme...");
+        try {
+            Thread.sleep((int)(balsa.getTiempo()));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("------------------------------------");
+    }
+
+    public synchronized void desembarcar(){
+        System.out.println("EN TIERRA FIRME");
+        System.out.println("Desembarcando pasajeros...");
+        System.out.println("Pasajeros salvados: ");
+        for (int i = 0; i < balsa.getCapacidad(); i++) {
+            System.out.println("/t"+balsa.getPasajeros().get(i)+" baja de la balsa "+balsa); //no tengo muy claro de que esto esté bien
+            balsa.bajarPasajeroBalsa(balsa.getPasajeros().get(i)); //tampoco lo tengo claro
+        }
+    }
+
+    public synchronized void volviendoABarco(){
+        System.out.println("La balsa "+balsa+" vuelve al barco a comprobar si quedan pasajeros.");
+        try {
+            Thread.sleep((int)(balsa.getTiempo()));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
