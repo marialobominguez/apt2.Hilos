1885f306ea016b98a142fce247602af27c4bd3ba
marialobominguez <marialobominguez@gmail.com>
2025-11-19 19:53:06 +0100

    he eliminado el 'synchronized' de todos los métodos salvo el de embarcar(), dado que es el único que puede dar problemas si dos balsas intentan acceder al barco a la vez

--- src/Rescate.java
+++ src/Rescate.java
@@ -43,7 +43,7 @@ public class Rescate extends Thread{
         }
     }
 
-    public synchronized void navegandoATierra(){
+    public void navegandoATierra(){
         System.out.println(balsa.getNombre()+" está navegando a tierra firme...");
         try {
             Thread.sleep((int)(balsa.getTiempo()));
@@ -53,7 +53,7 @@ public class Rescate extends Thread{
         System.out.println("------------------------------------");
     }
 
-    public synchronized void desembarcar(){
+    public void desembarcar(){
         System.out.println(balsa.getNombre()+" ESTÁ EN TIERRA FIRME");
         System.out.println("Desembarcando pasajeros...");
         System.out.println("Pasajeros salvados: ");
@@ -64,7 +64,7 @@ public class Rescate extends Thread{
         System.out.println("------------------------------------");
     }
 
-    public synchronized void volviendoABarco(){
+    public void volviendoABarco(){
         System.out.println("La balsa "+balsa.getNombre()+" vuelve al barco a comprobar si quedan pasajeros.");
         try {
             Thread.sleep((int)(balsa.getTiempo()));
